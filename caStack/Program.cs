08ed89ba473f1816838cef15778d3fa4c6d4a500
duh-h <cadugon1000@yahoo.com>
2023-11-13 16:39:28 -0300

    Adicionar arquivos de projeto.

---
+++ caStack/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace caStack
+{
+    internal class Program
+    {
+        public bool IsValid(string s)
+        {
+            Stack<char> openingParentheses = new Stack<char>();
+
+            foreach (char c in s)
+            {
+                if (c == '(' || c == '{' || c == '[' || c == ')' || c == '}' || c == ']')
+                {
+
+                    if (c == '(' || c == '{' || c == '[')
+                    {
+                        openingParentheses.Push(c);
+                    }
+                    else
+                    {
+                        if (openingParentheses.Count == 0) return false;
+
+                        char opening = openingParentheses.Pop();
+
+                        if (opening == '(' && c != ')'
+                            || opening == '{' && c != '}'
+                            || opening == '[' && c != ']') return false;
+                    }
+                }
+            }
+
+
+            return openingParentheses.Count == 0;
+        }
+   
+
+        static void Main(string[] args)
+        {
+            // Example usage of IsValid method
+            string testString1 = "{5*[a+b]-d*(c-a) + log10}";
+            string testString2 = "{5*[a+b]-d*(c-a] + log10}";
+
+
+            Program program = new Program();
+
+            Console.WriteLine($"'{testString1}' valido? {program.IsValid(testString1)}");
+            Console.WriteLine($"'{testString2}' valido? {program.IsValid(testString2)}");
+
+        }
+    }
+}
---
+++ caStack/stack.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caStack
+{
+    internal class Stack<T>
+    {
+        public Noh<T> Head { get; set; }
+        public int Count { get; private set; }
+
+        public Stack()
+        {
+            this.Head = new Noh<T>(default(T));
+            this.Head.anterior = null;
+            this.Head.proximo = null;
+            this.Count = 0;
+        }
+
+        public void Push(T info)
+        {
+            Noh<T> novoNoh = new Noh<T>(info);
+
+            if (this.Head.proximo == null)
+            {
+                this.Head.proximo = novoNoh;
+                this.Head.anterior = novoNoh;
+            }
+            else
+            {
+                novoNoh.anterior = this.Head.anterior;
+                this.Head.anterior.proximo = novoNoh;
+                this.Head.anterior = novoNoh;
+            }
+
+            Count++;
+        }
+
+        public T Pop()
+        {
+            if (Count > 0)
+            {
+                T poppedValue = Head.anterior.info;
+                Head.anterior = Head.anterior.anterior;
+
+                if (Head.anterior != null)
+                {
+                    Head.anterior.proximo = null;
+                }
+                else
+                {
+                    Head.proximo = null;
+                }
+
+                Count--;
+
+                return poppedValue;
+            }
+            else
+            {
+                throw new InvalidOperationException("Stack is empty. Cannot pop.");
+            }
+        }
+    }
+}
